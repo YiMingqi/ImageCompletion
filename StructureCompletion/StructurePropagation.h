965baf3fb7ce5f90df5d63b9ce4bf1a4ad70f61f
ymq <zju_ymq@163.com>
2018-05-22 21:05:14 +0800

    init

---
+++ StructureCompletion/StructurePropagation.h
@@ -0,0 +1,11 @@
+#pragma  once
+#include "OpenCvUtility.h"
+class StructurePropagation
+{
+public:
+	~StructurePropagation(){}
+	void Run(const Mat1b &_mask,const Mat& _img,const vector<Point> &points,Mat& result);
+	//利用目前类中已经存储的数据继续经行修补
+	//void RunAgain();
+	void SetParm(int _blocksize,int _samplestep,int _iscurve);
+};
